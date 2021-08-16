d4d445c68d2d22c713632d1b8b2f327f965a9be0
Bliss Pisit Wetcha <pisit@nimblehq.co>
2021-08-16 11:13:37 +0700

    [#5] Add POC for condition request

---
+++ POC/GithubAPIConditionRequest.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+
+    var lastUpdate: String?
+
+    override func loadView() {
+        let mainView = UIView()
+        mainView.backgroundColor = .systemGray5
+        mainView.isUserInteractionEnabled = true
+        mainView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapScreen)))
+        self.view = mainView
+    }
+
+    @objc func didTapScreen() {
+        fetchNotifications()
+    }
+
+    func fetchNotifications() {
+        let url = URL(string: "https://api.github.com/notifications")!
+
+        var request = URLRequest(url: url)
+        request.setValue("token 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", forHTTPHeaderField: "Authorization")
+        request.setValue(lastUpdate, forHTTPHeaderField: "If-Modified-Since")
+        request.cachePolicy = .reloadIgnoringCacheData
+        let task = URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
+            guard let data = data else { return }
+            print(String(data: data, encoding: .utf8)!)
+            if let httpResponse = response as? HTTPURLResponse {
+                print("status code \(httpResponse.statusCode)")
+                 if let lastUpdate = httpResponse.allHeaderFields["Last-Modified"] as? String {
+                    self?.lastUpdate = lastUpdate
+                    lastUpdate
+                 }
+            }
+        }
+        task.resume()
+    }
+}
+
+PlaygroundPage.current.liveView = MyViewController()
