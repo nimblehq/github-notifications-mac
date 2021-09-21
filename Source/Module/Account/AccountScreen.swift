f3f3f35fa94925fce855705cb5f2fb948a559d37
MarkG <markgravity.in@gmail.com>
2021-09-21 15:09:04 +0700

    [#14] Integrate Account Screen

--- Source/Module/Account/AccountScreen.swift
+++ Source/Module/Account/AccountScreen.swift
@@ -9,17 +9,47 @@
 import SwiftUI
 
 struct AccountScreen: View {
-    
+
+    @ObservedObject private var viewModel = AccountViewModel(
+        userService: NetworkServiceFactory.shared.createUserService(),
+        userSession: UserSessionStorage()
+    )
+    @Binding private var isLoggedIn: Bool
+
     var body: some View {
         VStack(alignment: .leading) {
-            // TODO: Update with real data in integrate
+            if viewModel.isFetching {
+                if viewModel.isFetchingFailed {
+                    Text("Unable to fetch user information")
+                } else {
+                    ProgressView()
+                }
+            } else {
+                content
+            }
+            Spacer()
+        }
+        .padding(.horizontal, 20.0)
+        .padding(.top, 50.0)
+        .navigationTitle("Account")
+        .foregroundColor(.almostBlack)
+        .onAppear {
+            viewModel.fetch()
+        }
+    }
+
+    var content: some View {
+        Group {
             Text("Username")
                 .foregroundColor(.brightGray)
             HStack {
-                Text("@suho")
+                Text("@\(viewModel.username)")
                     .font(.body.bold())
                 Spacer()
-                Button {} label: {
+                Button {
+                    viewModel.signOut()
+                    isLoggedIn = false
+                } label: {
                     Text("Sign Out")
                         .frame(maxWidth: 160.0)
                 }
@@ -31,17 +61,16 @@ struct AccountScreen: View {
                     )
                 )
             }
-            Spacer()
         }
-        .padding(.horizontal, 20.0)
-        .padding(.top, 50.0)
-        .navigationTitle("Account")
-        .foregroundColor(.almostBlack)
+    }
+
+    init(isLoggedIn: Binding<Bool>) {
+        _isLoggedIn = isLoggedIn
     }
 }
 
 struct AccountScreen_Previews: PreviewProvider {
     static var previews: some View {
-        AccountScreen()
+        AccountScreen(isLoggedIn: .constant(true))
     }
 }
