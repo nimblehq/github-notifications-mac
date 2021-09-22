f30d821aadbc73bae0952d45a97e7e61836647d6
Su T. Nguyen <vnntsu@gmail.com>
2021-09-22 13:23:50 +0700

    [25] Remove redundant codes

--- Source/Module/Repository/RepositoryViewModel.swift
+++ Source/Module/Repository/RepositoryViewModel.swift
@@ -13,11 +13,8 @@ final class RepositoryViewModel: ObservableObject {
     @Published var cellViewModels: [RepositoryCellViewModel] = []
     @Published var notifiedRepositoryObservable = NotifiedRepositoryObservable()
 
-    private var cancellableBag = Set<AnyCancellable>()
-
     func updateIfNeeded() {
         cellViewModels = notifiedRepositoryObservable.getNotifiedRepositories()
-        print("cellViewModels: \(cellViewModels.map { $0.name })")
     }
 
     func getIsNotifying(for repo: RepositoryCellViewModel) -> Bool {
