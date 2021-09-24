a63e1856b9362951e6ecf50c24b748036816a8be
llleyelll <chida.fua@gmail.com>
2021-09-24 12:02:16 +0700

    [#100] Handle notification subject type with camel case

---
+++ Source/Extensions/String+CamelCase.swift
@@ -0,0 +1,28 @@
+//
+//  String+CamelCase.swift
+//  Github Notifications
+//
+//  Created by Chananchida F. on 9/24/21.
+//  Copyright © 2021 Nimblehq. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    var uppercasingFirst: String {
+        return prefix(1).uppercased() + dropFirst()
+    }
+
+    var lowercasingFirst: String {
+        return prefix(1).lowercased() + dropFirst()
+    }
+
+    var camelized: String {
+        guard !isEmpty else { return "" }
+        let parts = components(separatedBy: CharacterSet.alphanumerics.inverted)
+        let first = String(describing: parts.first!).lowercasingFirst
+        let rest = parts.dropFirst().map({String($0).uppercasingFirst})
+        return ([first] + rest).joined(separator: "")
+    }
+}
